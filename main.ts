8ade90f4ac9f983ff0b87d2384634ff41f74c612
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2024-09-10 17:11:09 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,66 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        bitbot.rotate(BBRobotDirection.Left, 80)
+    } else if (receivedNumber == 2) {
+        bitbot.rotate(BBRobotDirection.Right, 80)
+    } else if (receivedNumber == 3) {
+        bitbot.go(BBDirection.Forward, 80)
+    } else if (receivedNumber == 4) {
+        bitbot.go(BBDirection.Reverse, 80)
+    } else {
+        bitbot.go(BBDirection.Forward, 0)
+    }
+})
+bitbot.ledRainbow(true, BBArms.Both)
+let radioGruppe = 42
+radio.setGroup(radioGruppe)
+basic.showNumber(radioGruppe)
+basic.clearScreen()
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `,0)
+    } else if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(2)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `,0)
+    } else if (input.rotation(Rotation.Pitch) < -30) {
+        radio.sendNumber(3)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `,0)
+    } else if (input.rotation(Rotation.Pitch) > 30) {
+        radio.sendNumber(4)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `,0)
+    } else {
+        radio.sendNumber(0)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `,0)
+    }
+})
